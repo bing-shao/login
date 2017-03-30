7b2d85ce67537ec9fea608e810cfb6cec73dea26
shaobing <759041292@qq.com>
2017-03-30 13:10:09 +0800

    login

---
+++ js/login.js
@@ -0,0 +1,93 @@
+
+//google adverse
+//document.writeln('<div style="width:970px;margin:auto;margin-bottom:10px;">');
+//document.writeln('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+//document.writeln('<!-- 970_90 -->');
+//document.writeln('<ins class="adsbygoogle"');
+//document.writeln(' style="display:inline-block;width:970px;height:90px"');
+//document.writeln('  data-ad-client="ca-pub-4168987395515775"');
+//document.writeln(' data-ad-slot="3016453476"></ins>');
+//document.writeln('<script>');
+//document.writeln('(adsbygoogle = window.adsbygoogle || []).push({});');
+//document.writeln('</script>');
+//document.writeln('</div>');
+
+
+//google adverse
+document.writeln('<link href="/Content/css/demo.css" rel="stylesheet">');
+document.writeln('<div class="fdad">');
+document.writeln('<div class="fdr" onclick="this.parentNode.style.display =\'none\'">×</div>');
+document.writeln('<div>');
+document.writeln('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+document.writeln('<!-- 336_280 -->');
+document.writeln('<ins class="adsbygoogle"');
+document.writeln(' style="display:inline-block;width:336px;height:280px"');
+document.writeln(' data-ad-client="ca-pub-4168987395515775"');
+document.writeln(' data-ad-slot="5458875874"></ins>');
+document.writeln('<script>');
+document.writeln('(adsbygoogle = window.adsbygoogle || []).push({});');
+document.writeln('</script>');
+document.writeln('</div>');
+document.writeln('</div>');
+
+
+//baidu 
+document.writeln('<!--百度统计-->');
+document.writeln('<div style="display: none">');
+document.writeln('<script type="text/javascript">');
+document.writeln('var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");');
+document.writeln("document.write(unescape(\"%3Cscript src='\" + _bdhmProtocol + \"hm.baidu.com/h.js%3Fda805d06f22ad936dc01c183988e5ef4' type='text/javascript'%3E%3C/script%3E\"));");
+document.writeln('</script>');
+document.writeln('</div>');
+document.writeln('<!--/百度统计-->');
+
+
+//baidu union
+//document.writeln('<script type="text/javascript">');
+//document.writeln(' var cpro_id = "u1673276";');
+//document.writeln('</script>');
+//document.writeln('<script src="http://cpro.baidustatic.com/cpro/ui/f.js" type="text/javascript"></script>');
+
+/*120*270 创建于 2014-08-22*/
+
+
+
+//访问更多
+
+//document.writeln('<div class="more" style="width:970px;margin:auto;margin-bottom:10px;">');
+//document.writeln('<a target="_blank" href="http://www.w2bc.com/shili" style="display: block; width: 970px; margin: auto; height: 40px; line-height: 40px; text-align: center; background: #7FB41D;font-size: 18px; color: #FFF; text-decoration: none">点击这里查看更多实例 </a>');
+//document.writeln('</div>');
+
+
+
+//前端加我
+//document.writeln('<div id="addme" style="position:fixed; right:10px;bottom:20px;border:solid 1px #399CEE; padding:5px;z-index:99999;display:none;">前端加我</div>');
+//document.writeln('<script src="http://www.w2bc.com/Scripts/jquery-1.11.3.js"></script>');
+//document.writeln('<script src="http://www.w2bc.com/Scripts/jquery-confirm.min.js"></script>');
+//document.writeln('<script src="http://www.w2bc.com/Scripts/jquery.cookie.js"></script>');
+//function AddMe() {
+//    msg = "<img style=\"width:200px;display:block;margin:auto;\"  src =\"http://pic.w2bc.com/upload/Content/pic/mmexport1465223430784.jpg\" />";
+//    $.alert({
+//        title: '从事前端的加我微信，一起交流',
+//        content: msg,
+//        confirmButton: false,
+//        closeIcon: true,
+//        cancel: function () {
+//            $.cookie("demoshowtwocode", "yes", { expires: 1 / 24 / 12 });
+//            $("#addme").show();
+//        }
+//    });
+//}
+
+
+//if (getCookie("demoshowtwocode") == null) {
+//    AddMe();
+//}
+//function getCookie(name) {
+//    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+//    if (arr = document.cookie.match(reg))
+//        return unescape(arr[2]);
+//    else
+//        return null;
+//}
+
